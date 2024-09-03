57bb801a72c75c9cf64d2840774bccc8373979e0
headevs <106855879+eduardodfran@users.noreply.github.com>
2024-09-03 16:27:51 +0800

    Update NavBar.jsx

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     <nav className="navbar">
       <Link to="/"><img src={logo} alt="Logo" /></Link>
       <ul>
-        <li><a to="/blog">Blog</a></li>
+        <li><Link to="/blog">Blog</Link></li>
         <li><Link to="/journal">Journal</Link></li>
       </ul>
     </nav>
